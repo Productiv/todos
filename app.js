dbe7b1958635c35109fd3494210747a5e1720158
Jeremy Meyer <jjman505@gmail.com>
2014-12-30 01:27:53 -0500

    added node_modules to gitignore

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var config = require('./config');
+var routes = require('./routes/todos');
 
 var app = express();
+
+// Configure
+mongoose.connect(config.db.url);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,8 +26,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/api', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
